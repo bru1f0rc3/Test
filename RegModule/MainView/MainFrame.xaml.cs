8bbf72ade10514c5c5ae5dbbb7dfc5257ed5245e
bru1f0rc3 <wint1841@inbox.ru>
2024-07-11 22:32:54 +0300

    Добавьте файлы проекта.

---
+++ RegModule/MainView/MainFrame.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using RegModule.Pages;
+
+namespace RegModule.MainView
+{
+    /// <summary>
+    /// Логика взаимодействия для MainFrame.xaml
+    /// </summary>
+    public partial class MainFrame : Page
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddUserPage());
+        }
+
+        private void WatchUsersButtoon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddModuleforUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
